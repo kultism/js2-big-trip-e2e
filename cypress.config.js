61ba2ea57ea190d0bd875949637a4d98a0d79315
Elena Antonova <antonova.elena.khv@gmail.com>
2024-04-22 14:13:16 +0300

    Изменит ссылку

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     excludeSpecPattern: '*.js',
     specPattern: 'cypress/integration/**/*.{feature,features}',
     baseUrl: 'http://localhost:8080',
-    baseApiUrl: 'https://22.objects.htmlacademy.pro/big-trip',
+    baseApiUrl: 'https://23.objects.htmlacademy.pro/big-trip',
     viewportHeight: 1000,
     viewportWidth: 1280,
     experimentalWebKitSupport: true,
